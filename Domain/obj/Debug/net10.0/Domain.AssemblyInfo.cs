a34966ef5fc2ecefd46312f500cc5a0992024290
Jhoan2007MA <217262098+Jhoan2007MA@users.noreply.github.com>
2026-06-01 08:07:34 -0500

    feat: :sparkles: Commit 7
    Creacion de ValueObjects en Domain

--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c34a25754a99fa0b1224673a69c219a799cbbc05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac2784054db64e6dc361ee08d3a80a4cf464aaf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
