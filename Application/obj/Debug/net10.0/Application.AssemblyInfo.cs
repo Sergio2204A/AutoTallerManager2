e0b4c7a9242f40cb3ce4b8d046e73a330f49b7eb
Jhoan2007MA <217262098+Jhoan2007MA@users.noreply.github.com>
2026-06-01 07:58:41 -0500

    feat: :sparkles: Commit 4
    Implementacion de archivos de Application

--- Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+176aadc38b6cdac544e5aa00eb19082ab8fd4f58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33f04d1357b1ca9529da506c64a7bdee60aa0815")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
